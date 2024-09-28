f24bb6b8c5ab57d35eee86d8364152d039e055a0
kabdelhalem <kareem.weirdo@gmail.com>
2024-09-28 12:24:58 -0400

    Initial Commit

---
+++ TrekStarIOS/TrekStarIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekStarIOSApp.swift
+//  TrekStarIOS
+//
+//  Created by Kareem Abdelhalem on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TrekStarIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
